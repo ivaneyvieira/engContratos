ae66c9f6fb5711cca7605beb818c89f651c31e84
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-05-09 11:04:26 -0300

    Commit inicial

---
+++ src/main/kotlin/br/com/astrosoft/AppConfig.kt
@@ -0,0 +1,19 @@
+package br.com.astrosoft
+
+import br.com.astrosoft.engContratos.model.saci
+import br.com.astrosoft.framework.model.IUser
+import br.com.astrosoft.framework.spring.SecurityUtils
+import br.com.astrosoft.framework.view.ViewUtil
+
+object AppConfig {
+  val version = ViewUtil.versao
+  const val appName = "devFornecedor"
+  const val commpany = "Engecopi"
+  const val title = "Devolução Fornecedor"
+  const val shortName = "Devolução"
+  const val iconPath = "icons/logo.png"
+  val userDetails get() = SecurityUtils.userDetails
+  val user get() = userDetails?.userSaci
+  val isAdmin get() = user?.admin == true
+  fun findUser(username: String?): IUser? = saci.findUser(username)
+}
