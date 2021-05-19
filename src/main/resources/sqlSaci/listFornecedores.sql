9d8b0048f9dc0e21a5a2db0fe6d4ef12db5f830f
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-05-19 06:24:50 -0300

    Ajuste no sql

--- src/main/resources/sqlSaci/listFornecedores.sql
+++ src/main/resources/sqlSaci/listFornecedores.sql
@@ -7,12 +7,12 @@ SELECT no                            AS vendno,
        email                         AS email,
        auxChar1                      AS nomeFantasia,
        IFNULL(RV.rmk, '')            AS rmkVend,
-       MAX(CAST(issue_date AS DATE)) AS ultimaData
-FROM sqldados.vend             AS V
+       CAST(MAX(issue_date) AS DATE) AS ultimaData
+FROM sqldados.vend              AS V
   INNER JOIN sqldados.inv       AS I
-	      ON I.vendno = V.no
-  LEFT JOIN sqldados.nfvendRmk AS RV
-	      ON RV.vendno = V.no AND RV.tipo = '99'
+	       ON I.vendno = V.no
+  LEFT JOIN  sqldados.nfvendRmk AS RV
+	       ON RV.vendno = V.no AND RV.tipo = '99'
 WHERE terms LIKE 'CONTRATO%'
   AND (no = @LOC * 1 OR name LIKE @LOCLIKE OR sname LIKE @LOCLIKE OR auxChar1 LIKE @LOCLIKE OR
        TRIM(@LOC) = '')
