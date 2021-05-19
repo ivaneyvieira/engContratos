b472fabd36fe1903b28f724ef9c3a15a9e4947de
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-05-19 06:20:50 -0300

    Erro de converção

--- src/main/kotlin/br/com/astrosoft/framework/model/LocalDateConverter.kt
+++ src/main/kotlin/br/com/astrosoft/framework/model/LocalDateConverter.kt
@@ -1,5 +1,6 @@
 package br.com.astrosoft.framework.model
 
+import br.com.astrosoft.framework.util.toLocalDate
 import org.sql2o.converters.Converter
 import org.sql2o.converters.ConverterException
 import java.sql.Date
@@ -8,8 +9,11 @@ import java.time.ZoneOffset
 
 class LocalDateConverter : Converter<LocalDate?> {
   @Throws(ConverterException::class) override fun convert(value: Any?): LocalDate? {
-    if (value !is Date) return null
-    return value.toLocalDate()
+    return when (value) {
+      is Date           -> value.toLocalDate()
+      is java.util.Date -> value.toLocalDate()
+      else              -> null
+    }
   }
 
   override fun toDatabaseParam(value: LocalDate?): Any? {
